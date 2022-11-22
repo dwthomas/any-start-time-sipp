02030470ecbaa0a5c77fabe334ce2bf993f3e696
Devin Thomas <devin.w.thomas@gmail.com>
2022-11-22 18:54:25 -0500

    sh - slurm experiment script

--- scripts/runexp.sh
+++ scripts/runexp.sh
@@ -4,4 +4,5 @@
 #SBATCH --mail-user=dwt@cs.unh.edu   # Where to send mail	
 #SBATCH --ntasks=1                  # Run a single task
 #SBATCH --array=1-15                 # Array range
+#SBATCH -p compute
 eval "$(head -${SLURM_ARRAY_TASK_ID} $1) | tail -1"
