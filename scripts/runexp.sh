38c14b26d0e80828392e7cc8edaa2ff15fbed8c7
Devin Thomas <devin.w.thomas@gmail.com>
2022-11-22 18:51:59 -0500

    sh - slurm experiment script

--- scripts/runexp.sh
+++ scripts/runexp.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+#SBATCH --job-name=sipptest   # Job name
+#SBATCH --mail-type=NONE            # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=dwt@cs.unh.edu   # Where to send mail	
+#SBATCH --ntasks=1                  # Run a single task
+#SBATCH --array=1-15                 # Array range
 eval "$(head -${SLURM_ARRAY_TASK_ID} $1) | tail -1"
