d3330e0e80464eeca0cbf87f62071bb462e331b3
Devin Thomas <devin.w.thomas@gmail.com>
2022-11-23 16:40:39 -0500

    sh - preparing to run experiments

--- scripts/runexp.sh
+++ scripts/runexp.sh
@@ -3,8 +3,8 @@
 #SBATCH --mail-type=NONE            # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=dwt@cs.unh.edu   # Where to send mail	
 #SBATCH --ntasks=1                  # Run a single task
-#SBATCH --array=1-1120                 # Array range
-#SBATCH -p compute
+#SBATCH --array=1-24576                 # Array range
+#SBATCH -p spare
 cd /home/aifs2/devin/Documents/pdap/scripts/
 echo "$(head -${SLURM_ARRAY_TASK_ID} /home/aifs2/devin/Documents/pdap/scripts/commands.run | tail -1)"
 eval "$(head -${SLURM_ARRAY_TASK_ID} /home/aifs2/devin/Documents/pdap/scripts/commands.run | tail -1)"
