9bda46faa7c9fc5742922a5ca9c81cd1a45ae236
Devin Thomas <devin.w.thomas@gmail.com>
2022-11-27 19:20:46 -0500

    added timeout to slurm script

--- scripts/runexp.sh
+++ scripts/runexp.sh
@@ -4,6 +4,8 @@
 #SBATCH --mail-user=dwt@cs.unh.edu   # Where to send mail	
 #SBATCH --ntasks=1                  # Run a single task
 #SBATCH --array=1-3072                 # Array range
+#SBATCH --time=0:10:00
+#SBATCH --no-kill
 #SBATCH -p compute
 cd /home/aifs2/devin/Documents/pdap/scripts/
 echo "$(head -${SLURM_ARRAY_TASK_ID} /home/aifs2/devin/Documents/pdap/scripts/commands.run | tail -1)"
