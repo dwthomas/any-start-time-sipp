915bf4646a1dadc3a45c43bed1e28a1c16e9d74e
Devin Thomas <devin.w.thomas@gmail.com>
2022-12-08 11:18:00 -0500

    cpp - rewrote search to use interval begin and end

--- include/search.hpp
+++ include/search.hpp
@@ -102,8 +102,8 @@ inline void sippGenerateSuccessors(std::size_t cnode, const State& goal, double 
             safe_intervals.waits(act, current_node.intervalInd, destination_ind, edge_ind, dt);
             for (std::size_t i = 0; i < edge_ind.size(); i++){
                 const auto & edge_interval = safe_intervals.get_edge(act, current_node.intervalInd, destination_ind[i], edge_ind[i]);
-                act.destination.time = std::max(current_node.s.time + dt, edge_interval.first + dt); 
-                if (edge_interval.second < act.destination.time){
+                act.destination.time = std::max(current_node.s.time + dt, intervalBegin(edge_interval) + dt); 
+                if (intervalEnd(edge_interval) < act.destination.time){
                     continue;
                 }
                 assert(safe_intervals.valid(act, agent_speed));
@@ -200,13 +200,13 @@ inline void pdapGenerateSuccessors(std::size_t cnode, const State& goal, double 
             safe_intervals.waits(act, current_node.intervalInd, destination_ind, edge_ind, dt);
             for (std::size_t i = 0; i < edge_ind.size(); i++){
                 const safe_interval& edge_interval = safe_intervals.get_edge(act, current_node.intervalInd, destination_ind[i], edge_ind[i]);
-                double alpha = std::max(current_node.alpha, edge_interval.first - delta_prior);
-                act.destination.time = std::max(current_node.s.time + dt, edge_interval.first + dt); 
-                if (edge_interval.second < act.destination.time){
+                double alpha = std::max(current_node.alpha, intervalBegin(edge_interval) - delta_prior);
+                act.destination.time = std::max(current_node.s.time + dt, intervalBegin(edge_interval) + dt); 
+                if (intervalEnd(edge_interval) < act.destination.time){
                     continue;
                 }
                 assert(safe_intervals.valid(act, agent_speed));
-                double beta = std::min(current_node.beta, edge_interval.second - alpha - delta_prior);
+                double beta = std::min(current_node.beta, intervalEnd(edge_interval) - alpha - delta_prior);
                 double f = act.destination.time + eightWayDistance(act.destination, goal, agent_speed);
                 std::size_t node_ind = safe_intervals.visited(act, current_node.intervalInd, destination_ind[i], edge_ind[i]);
                 std::size_t intervalInd = destination_ind[i];
@@ -246,10 +246,9 @@ inline void pdapAStar(const State& start_state, const State& goal, double agent_
     std::vector<NodeOpen<pdapNode, NodeGreater<pdapNode>>::handle_type> handles;
     std::vector<bool> node_on_open;
     pdapNode::nodes.clear();
-    //boost::heap::priority_queue<std::size_t, boost::heap::compare<pdapNodeGreater>> open;
     auto start_interval = safe_intervals.get_interval(0.0, map.get_safe_interval_ind(start_state));
     double f = start_state.time + eightWayDistance(start_state, goal, agent_speed);
-    auto current_node = pdapNode::newNode(start_state.x, start_state.y, start_interval->first, start_state.time, 0.0, start_interval->second, 0.0, f, std::numeric_limits<std::size_t>::max());
+    auto current_node = pdapNode::newNode(start_state.x, start_state.y, intervalBegin(start_interval), start_state.time, 0.0, intervalEnd(start_interval), 0.0, f, std::numeric_limits<std::size_t>::max());
     handles.emplace_back(open.emplace(current_node));
     node_on_open.emplace_back(true);
     assert(handles.size() == pdapNode::nodes.size());
@@ -308,13 +307,13 @@ inline void partialPdapGenerateSuccessors(std::size_t cnode, const State& goal, 
             ++(partialPdapNode::nodes[cnode].expansions);
             for (std::size_t i = 0; i < edge_ind.size(); i++){
                 const safe_interval& edge_interval = safe_intervals.get_edge(act, current_node.intervalInd, destination_ind[i], edge_ind[i]);
-                double alpha = std::max(current_node.alpha, edge_interval.first - delta_prior);
-                act.destination.time = std::max(current_node.s.time + dt, edge_interval.first + dt); 
-                if (edge_interval.second < act.destination.time){
+                double alpha = std::max(current_node.alpha, intervalBegin(edge_interval) - delta_prior);
+                act.destination.time = std::max(current_node.s.time + dt, intervalBegin(edge_interval) + dt); 
+                if (intervalEnd(edge_interval) < act.destination.time){
                     continue;
                 }
                 assert(safe_intervals.valid(act, agent_speed));
-                double beta = std::min(current_node.beta, edge_interval.second - alpha - delta_prior);
+                double beta = std::min(current_node.beta, intervalEnd(edge_interval) - alpha - delta_prior);
                 double f = act.destination.time + eightWayDistance(act.destination, goal, agent_speed);
                 std::size_t node_ind = safe_intervals.visited(act, current_node.intervalInd, destination_ind[i], edge_ind[i]);
                 std::size_t intervalInd = destination_ind[i];
@@ -358,7 +357,7 @@ inline Functional partialPdapAStar(const State& start_state, const State& goal, 
     double max_query = metadata.args()["maxquery"].as<double>();
     auto start_interval = safe_intervals.get_interval(0.0, map.get_safe_interval_ind(start_state));
     double f = start_state.time + eightWayDistance(start_state, goal, agent_speed);
-    auto current_node = partialPdapNode::newNode(start_state.x, start_state.y, start_interval->first, start_state.time, 0, start_interval->second, 0.0, f, f, 0,std::numeric_limits<std::size_t>::max());
+    auto current_node = partialPdapNode::newNode(start_state.x, start_state.y, intervalBegin(start_interval), start_state.time, 0, intervalEnd(start_interval), 0.0, f, f, 0,std::numeric_limits<std::size_t>::max());
     handles.emplace_back(open.emplace(current_node));
     node_on_open.emplace_back(true);
     assert(handles.size() == partialPdapNode::nodes.size());
@@ -395,118 +394,3 @@ inline Functional partialPdapAStar(const State& start_state, const State& goal, 
     }
     return functional.finalize();
 }
-/*
-inline void partialPdapGenerateSuccessors(std::size_t cnode, const State& goal, double agent_speed, SafeIntervals& safe_intervals, const Map& map,
-                                           boost::heap::priority_queue<std::size_t, boost::heap::compare<partialPdapNodeGreater>>& open, 
-                                          std::vector<std::size_t>& destination_ind, std::vector<std::size_t>& edge_ind){
-    double dt;
-    const partialPdapNode& current_node = partialPdapNode::getNode(cnode);
-    double delta_prior = current_node.delta();
-    Action act(current_node.s, State(0, 0, 0));
-    //auto interval_starts = std::vector<std::pair<double, double>>();
-    double min_f = std::numeric_limits<double>::infinity();
-    for (int m_x = -1; m_x <=1; m_x++){
-        act.destination.x = current_node.s.x + m_x;
-        for (int m_y = -1; m_y <=1; m_y++){
-            if (m_x == 0 && m_y == 0){
-                continue;
-            }
-            else if (m_x == 0 || m_y == 0) {
-                dt = agent_speed;
-            }
-            else{
-                dt = sqrt2()*agent_speed;
-            }
-            act.destination.y = current_node.s.y + m_y;
-            if (!map.inBounds(act.destination.x, act.destination.y)){
-                continue;
-            }
-            act.destination.time = current_node.s.time + dt;
-            std::size_t loc_ind = safe_intervals.waits(map, act, waits_res, waits_res_intervals, edge_ind, current_node.expansions + 2);
-            std::size_t i = current_node.expansions;
-            double delta_prior = current_node.delta();
-            double alpha = std::max(current_node.alpha, waits_res[i]);
-            act.destination.time =  current_node.delta() + dt + alpha;
-            if (safe_intervals.isSafe(act, map, agent_speed)){ //&& !safe_intervals.markvisited(loc_ind, i)){
-                if (waits_res.size() > i){
-                    double intervalStart = waits_res_intervals[i].first;
-                    double intervalEnd = waits_res_intervals[i].second;
-                    double beta = std::min(current_node.beta, intervalEnd - delta_prior);
-                    if(beta < alpha){
-                        continue;
-                    }
-                    double f = act.destination.time + eightWayDistance(act.destination, goal, agent_speed);
-                    std::size_t node_ind = safe_intervals.visited(loc_ind, edge_ind[i]);
-                    if (node_ind != std::numeric_limits<std::size_t>::max()){
-                        if (partialPdapNode::getNode(node_ind).s.time > act.destination.time){
-                            partialPdapNode::set_arrival(node_ind, act.destination.time, alpha, beta, f, cnode);
-                            partialPdapNode::set_f(node_ind, f, 0);
-                            open.emplace(node_ind);
-                        }
-                        continue;
-                    }
-                    auto j = partialPdapNode::newNode(act.destination.x, act.destination.y, intervalStart, act.destination.time, alpha, beta, f, 0, cnode);
-                    open.emplace(j);
-                    safe_intervals.markvisited(loc_ind, edge_ind[i], j);
-                    if (waits_res.size() > i+1){
-                        alpha = std::max(current_node.alpha, waits_res[i+1]);
-                        act.destination.time =  current_node.delta() + dt +alpha;
-                        f = act.destination.time + eightWayDistance(act.destination, goal, agent_speed);
-                        if (f < min_f){
-                            min_f = f;
-                        }
-                    }
-                }
-            }
-            
-            
-        }
-    }
-    if (std::isfinite(min_f)){
-        partialPdapNode::set_f(cnode, min_f);
-        open.emplace(cnode);
-    }
-}
-
-inline Functional partialPdapAStar(const State& start_state, const State& goal, double agent_speed, SafeIntervals& safe_intervals, const Map& map, Metadata& metadata){
-    metadata.runtime.start();
-    std::vector<double> waits_res;
-    std::vector<safe_interval> waits_res_intervals;
-    std::vector<std::size_t> edge_ind;
-    boost::heap::priority_queue<std::size_t, boost::heap::compare<partialPdapNodeGreater>> open;
-    Functional functional;
-    auto start_interval = safe_intervals.get_interval(0.0, map.get_safe_interval_ind(start_state));
-    double f = start_state.time + eightWayDistance(start_state, goal, agent_speed);
-    auto current_node = partialPdapNode::newNode(start_state.x, start_state.y, start_interval->first, start_state.time, 0.0, start_interval->second, f, 0,std::numeric_limits<std::size_t>::max());
-    safe_intervals.markvisited(map.get_safe_interval_ind(start_state), 0, current_node);
-    open.emplace(current_node);
-    //double lower_bound = 0.0;
-    double max_query = metadata.args()["maxquery"].as<double>();
-    while(!open.empty()){
-        current_node = open.top();
-        auto cn = partialPdapNode::getNode(current_node);
-        open.pop();
-        //if (cn.f < lower_bound){
-        //    continue;
-        //}
-        //lower_bound = cn.f;
-        ++(metadata.expansions);
-        if (isGoal(cn, goal)){
-            pdap_backtrack_path<partialPdapNode>(current_node);
-            double alph = functional.emplace_back(cn.alpha, cn.beta, cn.delta(), current_node);
-            //std::cout <<"alpha: " << alph << ", second: " << start_interval->second << "\n";
-            //debug_open(open);
-            if (alph >= max_query){
-                std::cout << "full functional found\n";
-                break;
-            }
-        }
-        if (functional.check_back(current_node)){
-            partialPdapGenerateSuccessors(current_node, goal, agent_speed, safe_intervals, map, open, waits_res, waits_res_intervals, edge_ind);
-        }
-    }
-    metadata.runtime.stop();
-    pdap_backtrack_path<partialPdapNode>(functional.domain[0.0].node_ind);
-    return functional;
-}
-*/
--- scripts/runexp.sh
+++ scripts/runexp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=sippandpdaprooms   # Job name
+#SBATCH --job-name=peap1024   # Job name
 #SBATCH --mail-type=NONE            # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=dwt@cs.unh.edu   # Where to send mail	
 #SBATCH --ntasks=1                  # Run a single task
-#SBATCH --array=1-3072                 # Array range
+#SBATCH --array=1-768                 # Array range
 #SBATCH --time=0:10:00
 #SBATCH --no-kill
 #SBATCH -p compute
 cd /home/aifs2/devin/Documents/pdap/scripts/
-echo "$(head -${SLURM_ARRAY_TASK_ID} /home/aifs2/devin/Documents/pdap/scripts/commands.run | tail -1)"
-eval "$(head -${SLURM_ARRAY_TASK_ID} /home/aifs2/devin/Documents/pdap/scripts/commands.run | tail -1)"
+echo "$(head -${SLURM_ARRAY_TASK_ID} /home/aifs2/devin/Documents/pdap/scripts/new_partial_commands.run | tail -1)"
+eval "$(head -${SLURM_ARRAY_TASK_ID} /home/aifs2/devin/Documents/pdap/scripts/new_partial_commands.run | tail -1)"
